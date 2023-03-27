1be2635bc1b3f6271551eebffed1ac22e3eeb8eb
LorenzoLettieri <lorenzolettieri2@gmail.com>
2023-03-27 15:12:00 +0200

    aiuto

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
                 <form method="POST" action="{{route("login")}}">
                     @csrf
                     <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label">Email address</label>
+                      <label for="exampleInputEmail1" class="form-label">EMAIL</label>
                       <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                       <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
